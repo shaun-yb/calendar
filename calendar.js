5ff60dc8c895a777bf2baa657eef84a15683070e
Shaun <shauncarland@gmail.com>
2020-02-10 16:31:51 -0500

    Get basic calendar working

---
+++ calendar.js
@@ -0,0 +1,10 @@
+var CalendarFactory = (function() {
+    return function(month, year) {
+        this.month = month;
+        this.year = year;
+        
+        return {
+            
+        }
+    }
+})
